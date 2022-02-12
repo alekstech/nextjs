97b14649d32b72911a42f2d92b411f0966953f99
Aleks Sobieraj <z.sobieraj@gmail.com>
2022-02-12 12:20:00 -0500

    CSP

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-A minimal placeholder page hosted at aleks.tech.
+# aleks.tech
 
-Built in late 2021 using NextJS and TypeScript. Deployed using serverless to Lambda, API Gateway and CloudFront.
+This repository contains code for (aleks.tech)[https://aleks.tech].
+
+Built in 2022.
+
+[See stack](https://aleks.tech/blog/portfolio).
+
+
+## Developer notes
+
+- [Roadmap](https://aleks.tech/blog/portfolio#roadmap).
+- Run `npm run build` before the first `npm run dev`.
+- Markdown files are not hot reloaded. Run `npm run build` to see changes.
+- The `npm run deploy` script expects a .aws/config file with AWS credentials
--- next.config.js
+++ next.config.js
@@ -1,4 +1,73 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+// CSP directives: developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+const getContentSecurityPolicy = (phase) => {
+  const env = phase === PHASE_DEVELOPMENT_SERVER ? "dev" : "prod";
+  const directives = {
+    defaultSrc: {
+      prod: "'self'",
+      dev: "'unsafe-inline'"
+    },
+    scriptSrc: {
+      prod: "'self'",
+      dev: "'unsafe-inline'"
+    }
+  };
+
+  return `
+    default-src ${directives.defaultSrc[env]};
+    script-src ${directives.scriptSrc[env]};
+  `
+    .replace(/\s{2,}/g, ' ') // replace newline characters with a space
+    .trim();
+};
+
+const getHeaders = async function (phase) {
+  return [
+    {
+      source: '/:path*', // all routes
+      headers: [
+        {
+          key: 'X-DNS-Prefetch-Control', // proactively resolve DNS from links
+          value: 'on'
+        },
+        {
+          key: 'Strict-Transport-Security', // disallow HTTP connections without SSL
+          value: 'max-age=63072000; includeSubDomains' // for 2 years, on subdomains too
+        },
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff' // use the MIME types specified, don't infer from content
+        },
+        {
+          key: 'Content-Security-Policy',
+          value: getContentSecurityPolicy(phase)
+        },
+        {
+          key: 'X-XSS-Protection', // in case the CSP isn't understood
+          value: '1; mode=block'
+        },
+        {
+          key: 'X-Frame-Options',
+          value: 'SAMEORIGIN' // disallow i-framing the website
+        },
+        {
+          key: 'Referrer-Policy',
+          value: 'origin-when-cross-origin' // only send the hostname when user navigates away from this website
+        }
+      ],
+    },
+  ];
+};
+
+module.exports = (phase) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    reactStrictMode: true, // lint deprecated patterns
+    headers: getHeaders(phase),
+  };
+  return nextConfig;
 };
