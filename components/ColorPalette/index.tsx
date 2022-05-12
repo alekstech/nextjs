9237de42dc407ea012ef0e0cf308eb0fb2bd255f
Aleks Sobieraj <z.sobieraj@gmail.com>
2022-05-12 00:32:07 -0400

    Monk skintone scale

---
+++ components/ColorPalette/index.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const StyledFrame = styled.div`
+  display: flex;
+  width: 100%;
+  height: 1rem;
+`;
+
+interface StyledPanelProps {
+  colorValue?: string;
+}
+
+const StyledPanel = styled.div<StyledPanelProps>`
+  flex-grow: 1;
+  background-color: ${props => {
+    return props.colorValue || "";
+  }};
+`;
+
+type ColorPaletteProps = {
+  colors: string[];
+}
+
+export const ColorPalette = (props: ColorPaletteProps) => {
+  return (
+    <StyledFrame>
+      {props.colors.map(color => (
+        <StyledPanel colorValue={color} key={color} />
+      ))}
+    </StyledFrame>
+  );
+};
+
+export default ColorPalette;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,22 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import PrefetchedLink from '../components/PrefetchedLink';
+import ColorPalette from '../components/ColorPalette';
 import styles from './Home.module.css';
 import Cluster from '../components/Layout/cluster';
+
+const MonkSkintoneScale = [
+  "#f6ede4",
+  "#f3e7db",
+  "#f7ead0",
+  "#eadaba",
+  "#d7bd96",
+  "#a07e56",
+  "#825c43",
+  "#604134",
+  "#3a312a",
+  "#292420"
+];
 
 const Home: NextPage = () => {
   return (
@@ -17,6 +31,8 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Aleks Sobieraj
         </h1>
+
+        <ColorPalette colors={MonkSkintoneScale} />
 
         <p className={styles.description}>
           web developer
