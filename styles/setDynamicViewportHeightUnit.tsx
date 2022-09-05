f10bd578fbd79871dd42e44c2bb797ec36bbdc08
Aleks Sobieraj <z.sobieraj@gmail.com>
2022-09-04 23:40:26 -0400

    init dvh on load

--- styles/setDynamicViewportHeightUnit.tsx
+++ styles/setDynamicViewportHeightUnit.tsx
@@ -1,10 +1,12 @@
 export const setDynamicViewportHeightUnit = () => {
-  window.addEventListener('resize', () => {
-    requestAnimationFrame(() => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--dvh', `${vh}px`);
-    });
+  const setProperty = () => requestAnimationFrame(() => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--dvh', `${vh}px`);
   });
+  window.addEventListener('resize', () => {
+    setProperty();
+  });
+  setProperty();
 };
 
 export default setDynamicViewportHeightUnit;
