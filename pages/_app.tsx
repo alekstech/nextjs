551207d856103b64ec01823e1cbce1e5b855f964
Aleks Sobieraj <z.sobieraj@gmail.com>
2023-01-01 02:16:05 -0500

    test dvh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,22 +3,17 @@ import '../styles/index.css';
 import type { AppProps } from 'next/app';
 import { Provider as AuthProvider } from '../contexts/authentication';
 import usePreferredColorScheme from '../contexts/theme/usePreferredColorScheme';
-import setDynamicViewportHeightUnit from '../styles/setDynamicViewportHeightUnit';
 import asciiLogo from '../public/asciiLogo';
 
 const CustomApp = function({ Component, pageProps }: AppProps):ReactElement {
 
-  // Do things when page first loads in browser
   useEffect(() => {
     // Print a logo in console
     console.log(asciiLogo);
-    
-    // Match color scheme to user's system
-    usePreferredColorScheme();
-    
-    // Switch to dvh? In Safari since 2022
-    setDynamicViewportHeightUnit();
   }, []);
+
+  // Match color scheme to user's system
+  usePreferredColorScheme();
 
   return (
     <AuthProvider>
