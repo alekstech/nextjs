ce5b11964335eeb802d33f6284b0ee55128f80af
Aleks Sobieraj <z.sobieraj@gmail.com>
2023-01-01 01:11:24 -0500

    ascii logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,19 @@ import type { AppProps } from 'next/app';
 import { Provider as AuthProvider } from '../contexts/authentication';
 import usePreferredColorScheme from '../contexts/theme/usePreferredColorScheme';
 import setDynamicViewportHeightUnit from '../styles/setDynamicViewportHeightUnit';
+import asciiLogo from '../public/asciiLogo';
 
 const CustomApp = function({ Component, pageProps }: AppProps):ReactElement {
-  usePreferredColorScheme();
+
+  // Do things when page first loads in browser
   useEffect(() => {
+    // Print a logo in console
+    console.log(asciiLogo);
+    
+    // Match color scheme to user's system
+    usePreferredColorScheme();
+    
+    // Switch to dvh? In Safari since 2022
     setDynamicViewportHeightUnit();
   }, []);
 
